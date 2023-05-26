4118f1c778ec152dd952cbb8d4971c72cab2874c
Hiiro9Homura <granderaart@gmail.com>
2023-05-26 18:23:13 +0600

    west

---
+++ filing.py
@@ -0,0 +1,20 @@
+# file = open("new.txt", "r")
+# file.write("Hello, World\n")
+# file.write("Hi")
+# s = file.read()
+# print(s)
+# file.close()
+
+# login = input("Введите ваш логин: ")
+# password = int(input())
+
+# with open("users.txt", "a", encoding="utf-8") as file:
+#     file.write(f"Ваш логин - {login}, Ваш пароль {password}")
+
+# try:
+#     with open("new.txt", "r") as file:
+#         s = file.read()
+#         print(s)
+#         file.close()
+# except FileNotFoundError:
+#     print("Файл не найден")
