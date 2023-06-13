ca25f4bef52205c9e671a7264e1e3f5a5adfc9d3
Hiiro9Homura <granderaart@gmail.com>
2023-06-13 19:58:12 +0600

    Tuesday

--- oop.py
+++ oop.py
@@ -262,47 +262,42 @@
 #     animal.info()
 #     animal.make_sound()
 
-from math import pi
+# from math import pi
+# class Shape:
+#     def __init__(self, name):
+#         self.name = name
 
+#     def area(self):
+#         pass
 
-class Shape:
-    def __init__(self, name):
-        self.name = name
+#     def fact(self):
+#         return "I am a two-dimensional shape."
 
-    def area(self):
-        pass
+#     def __str__(self):
+#         return self.name
 
-    def fact(self):
-        return "I am a two-dimensional shape."
+# class Square(Shape):
+#     def __init__(self, length):
+#         super().__init__("Square")
+#         self.length = length
 
-    def __str__(self):
-        return self.name
+#     def area(self):
+#         return self.length**2
 
+#     def fact(self):
+#         return "Squares have each angle equal to 90 degrees."
 
-class Square(Shape):
-    def __init__(self, length):
-        super().__init__("Square")
-        self.length = length
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         super().__init__("Circle")
+#         self.radius = radius
 
-    def area(self):
-        return self.length**2
+#     def area(self):
+#         return pi*self.radius**2
 
-    def fact(self):
-        return "Squares have each angle equal to 90 degrees."
-
-
-class Circle(Shape):
-    def __init__(self, radius):
-        super().__init__("Circle")
-        self.radius = radius
-
-    def area(self):
-        return pi*self.radius**2
-
-
-a = Square(4)
-b = Circle(7)
-print(b)
-print(b.fact())
-print(a.fact())
-print(b.area())
+# a = Square(4)
+# b = Circle(7)
+# print(b)
+# print(b.fact())
+# print(a.fact())
+# print(b.area())
